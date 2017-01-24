1539190e7438ac5f70121d3e7b92af302f8ebb85
Scott Chow <scottpchow@umail.ucsb.edu>
2017-01-24 14:09:58 -0800

    Just brushing up on vectors and iterators.

--- brainfuckInterpreter.cpp
+++ brainfuckInterpreter.cpp
@@ -1,9 +1,17 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <iterator>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	cout << "Hello World\n";
+	vector<char> v;
+	v.push_back('0');
+	v.push_back('1');
+	vector<char>::iterator it = v.begin();
+
+	for (it; it != v.end(); it++)
+		cout << *it << endl;
+
 	return 1;
 }
